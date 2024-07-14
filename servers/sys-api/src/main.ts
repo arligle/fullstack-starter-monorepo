4627accb954fac4818b05e10dfbb36043c716ff8
arligle <0774543002@163.com>
2024-07-15 03:57:34 +0800

    feat(sys-api): add swagger api docs

--- servers/sys-api/src/main.ts
+++ servers/sys-api/src/main.ts
@@ -3,13 +3,25 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
+  // MVC 导入hbs模版引擎
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  // swagger api docs
+  const options = new DocumentBuilder()
+    .setTitle('FULL STACK NESTJS')
+    .setDescription('The API DOCUMENT')
+    .setVersion('1.0')
+    .addTag('Home')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
